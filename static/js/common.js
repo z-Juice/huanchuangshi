c5f80b5426c348935c46d17da8aad66de3ed5ae9
张志勇 <515357583@qq.com>
2019-05-20 18:06:48 +0800

    修改

---
+++ static/js/common.js
@@ -0,0 +1,13 @@
+//屏幕大于992
+// window.onresize = function () {
+//     resize()
+// }
+// function resize() {
+//     var rem = 0;
+//     var htmlDOM = document.documentElement;
+//     var currentWidth = htmlDOM.clientWidth;
+//     if (currentWidth > 992) {
+//         rem = currentWidth / 12;
+//         htmlDOM.style.fontSize = rem + 'px';
+//     }
+// }
